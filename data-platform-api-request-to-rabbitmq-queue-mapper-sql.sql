bff08a1e84946506fca1969f2024b19351fda415
Yurino Oga <yurino.o@latona.io>
2022-10-18 12:11:33 +0900

    release

--- data-platform-api-request-to-rabbitmq-queue-mapper-sql.sql
+++ data-platform-api-request-to-rabbitmq-queue-mapper-sql.sql
@@ -4,7 +4,7 @@ CREATE TABLE `data_platform_api_request_to_rabbitmq_queue_mapper`
   `APIType`                            varchar(20) NOT NULL,
   `RepositoryName`                     varchar(100) DEFAULT NULL,
   `PortNoInRabbitMQ`                   int(5) DEFAULT NULL,
-  `NameOfQueue`                        varchar(100) DEFAULT NULL,
+  `NameOfQueueFrom`                    varchar(100) DEFAULT NULL,
     PRIMARY KEY (`APIServiceName`, `APIType`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
